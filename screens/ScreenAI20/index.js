dade006928ee8d4cbbc6663dc80432605d3d0f8f
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-06 20:54:13 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI20/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
+
+const FeedbackScreen = () => {
+  const [feedback, setFeedback] = useState('');
+
+  const sendFeedback = () => {
+    Alert.alert('Feedback Sent', 'Thank you for your feedback!', [{
+      text: 'OK'
+    }]);
+    setFeedback('');
+  };
+
+  return <View style={styles.container}>
+      <TextInput style={styles.input} placeholder="Write your feedback here..." multiline numberOfLines={5} value={feedback} onChangeText={setFeedback} />
+      <Button title="Send" onPress={sendFeedback} />
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    textAlignVertical: 'top'
+  }
+});
+export default FeedbackScreen;
