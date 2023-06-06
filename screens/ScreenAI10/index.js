468ee4c99c29a1fbf878b4e10f75bbbc7093e588
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-06 19:18:21 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI10/index.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+const SignUpScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+  return <View style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
+      <TextInput style={styles.input} placeholder="Username" onChangeText={setUsername} value={username} />
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry onChangeText={setPassword} value={password} />
+      <View style={styles.checkboxContainer}>
+        <TouchableOpacity onPress={() => setIsChecked(!isChecked)}>
+          <Image style={styles.checkbox} source={{
+          uri: isChecked ? 'https://tinyurl.com/42evm3m3' : 'https://tinyurl.com/42evm3m3'
+        }} />
+        </TouchableOpacity>
+        <Text style={styles.checkboxText}>
+          I agree to the{' '}
+          <Text style={styles.link} onPress={() => {}}>
+            Terms and Conditions
+          </Text>{' '}
+          and{' '}
+          <Text style={styles.link} onPress={() => {}}>
+            Privacy Policy
+          </Text>
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+      <Text style={styles.socialText}>Or sign up with:</Text>
+      <View style={styles.socialContainer}>
+        <TouchableOpacity>
+          <Image style={styles.socialIcon} source={{
+          uri: 'https://tinyurl.com/42evm3m3'
+        }} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.socialIcon} source={{
+          uri: 'https://tinyurl.com/42evm3m3'
+        }} />
+        </TouchableOpacity>
+      </View>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#CCCCCC',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    marginRight: 5
+  },
+  checkboxText: {
+    fontSize: 14
+  },
+  link: {
+    color: '#007AFF'
+  },
+  button: {
+    width: '80%',
+    height: 40,
+    backgroundColor: '#007AFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 10
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16
+  },
+  socialText: {
+    fontSize: 14,
+    marginBottom: 5
+  },
+  socialContainer: {
+    flexDirection: 'row'
+  },
+  socialIcon: {
+    width: 40,
+    height: 40,
+    marginRight: 10
+  }
+});
+export default SignUpScreen;
