5081b1ff3a8f6ae0ae71edf0504dd597cc7722e3
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-06 19:00:20 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI3/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import { View, Text, Image, StyleSheet, StatusBar, Dimensions } from 'react-native';
+
+const SplashScreen = ({
+  navigation
+}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Login');
+    }, 3000);
+  }, []);
+  return <View style={styles.container}>
+      <StatusBar hidden />
+      <Image source={{
+      uri: 'https://tinyurl.com/42evm3m3'
+    }} style={styles.backgroundImage} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Welcome to MyApp</Text>
+        <Image source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} style={styles.logo} />
+      </View>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  backgroundImage: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    position: 'absolute'
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20
+  },
+  logo: {
+    width: 100,
+    height: 100
+  }
+});
+export default SplashScreen;
