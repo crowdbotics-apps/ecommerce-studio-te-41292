fea2f62b323857bcb651f810b465ec6764a7a231
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-06 22:53:23 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "ecommerce-studio-te-41292",
-  url: "https://ecommerce-studio-te-41292.botics.co",
-  api: "https://ecommerce-studio-te-41292.botics.co/api/v1"
+  "name": "ecommerce-studio-te-41292",
+  "url": "https://ecommerce-studio-te-41292.botics.co",
+  "api": "https://ecommerce-studio-te-41292.botics.co/api/v1",
+  "initialRoute": "ScreenAI2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
