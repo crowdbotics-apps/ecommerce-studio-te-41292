4c72b8e9e024b45818321cc2a7813eb89d5db901
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-06 17:04:10 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "ecommerce-studio-te-41292",
   url: "https://ecommerce-studio-te-41292.botics.co",
   api: "https://ecommerce-studio-te-41292.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
